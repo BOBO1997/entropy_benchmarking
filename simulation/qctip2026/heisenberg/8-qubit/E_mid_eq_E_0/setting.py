8168798b2bb4b1633bf4a78a5dbe73dbc6a7f14a
Bo Yang <yh198595@gmail.com>
2025-12-25 14:16:04 +0100

    changed the range of N_shots and p

--- simulation/qctip2026/heisenberg/8-qubit/E_mid_eq_E_0/setting.py
+++ simulation/qctip2026/heisenberg/8-qubit/E_mid_eq_E_0/setting.py
@@ -12,9 +12,9 @@ Gammas_allowed = np.array([1.0, 1.1, 1.2, 1.5, 2.0, 3.0, 10.0])
 # gap = 1e-1
 gap = 1e-4
 
-Ns_shots = np.logspace(0, 7, num=71)
+Ns_shots = np.logspace(3, 10, num=71)
 
 ### depolarising rate per layer
-ps_dep_global = np.logspace(-7, 0, num=71)
+ps_dep_global = np.logspace(-10, -3, num=71)
 # Ps_dep_global = 1 - (1 - np.logspace(-7, 0, num=100)) ** num_layers
 
